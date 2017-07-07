67af0d4282139e7c5757727f8006d8f79b29f587
Pierre Trouve <pierre.trouve@inovia-team.com>
2017-07-07 16:17:40 +0200

    ok

--- laurent.py
+++ laurent.py
@@ -18,25 +18,21 @@ from nltk.stem.porter import *
 
 #print "bonjour Axel"
 
-counter = 0
 
-stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours',
-                 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers',
-                 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves',
-                 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are',
-                 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does',
-                 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until',
-                 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into',
-                 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down',
-                 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here',
-                 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',
-                 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so',
-                 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
+
 def rmP(x):
-    if counter > 2:
-        return 'test'
-    else:
-        counter +=1
+        stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours',
+                     'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers',
+                     'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves',
+                     'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are',
+                     'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does',
+                     'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until',
+                     'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into',
+                     'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down',
+                     'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here',
+                     'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',
+                     'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so',
+                     'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
         p=set(string.punctuation)
         doc=''.join([c for c in str(x).lower() if c not in p ])
         words=doc.split()

a129e0e7d3b821ebc6db904d5716df6d88ffcbbb
Pierre Trouve <pierre.trouve@inovia-team.com>
2017-07-07 15:24:21 +0200

    ok

--- laurent.py
+++ laurent.py
@@ -10,35 +10,6 @@ from pyspark.ml.linalg import DenseVector
 from pyspark.sql import Row
 from functools import partial
 from pyspark.ml.regression import LinearRegression
-from check import *
-import string
-import nltk
-from nltk.stem.porter import *
-
-!pip install -U nltk
-
-
-stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours',
-                 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers',
-                 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves',
-                 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are',
-                 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does',
-                 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until',
-                 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into',
-                 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down',
-                 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here',
-                 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',
-                 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so',
-                 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
-def rmP(x):
-    p=set(string.punctuation)
-    doc=''.join([c for c in str(x).lower() if c not in p ])
-    words=doc.split()
-    doc =[ word for word in words if word not in stopwords  ]
-    stemmer=PorterStemmer()
-    for i,word in enumerate(doc):
-        doc[i]=stemmer.stem(word.decode('utf-8'))
-    return ' '.join(doc)
 
 
 def fixEncoding(x):
@@ -75,7 +46,7 @@ def addFeatureLen(row):
 
 def cleanData(row, model):
     # we are going to fix search term field
-    text = rmP(row['search_term']).split()
+    text = row['search_term'].split()
     for i, v in enumerate(text):
         text[i] = correct(v, model)
     data = row.asDict()
@@ -189,4 +160,4 @@ lrModel = lr.fit(sqlContext.createDataFrame(train))
 result = lrModel.transform(sqlContext.createDataFrame(test))
 # Compute mean squared error metric
 MSE = result.rdd.map(lambda r: (r['label'] - r['prediction']) ** 2).mean()
-print("Mean Squared Error is : " + str(MSE))
+print("Mean Squared Error = " + str(MSE))
